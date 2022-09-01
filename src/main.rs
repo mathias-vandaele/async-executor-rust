e607f11013a8059d3fd332465942da99b93ad452
Mathias Vandaele <contactvandaele@gmail.com>
2022-09-01 19:28:40 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+mod timer_future;
+mod executor;
+
+use std::time::Duration;
+use crate::executor::new_executor_and_spawner;
+use crate::timer_future::TimerFuture;
+
+fn main() {
+    let (executor, spawner) = new_executor_and_spawner();
+
+    // Spawn a task to print before and after waiting on a timer.
+    spawner.spawn(async {
+        println!("howdy!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("done!");
+    });
+
+    spawner.spawn(async {
+        println!("howdy!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("done!");
+    });
+
+    // Drop the spawner so that our executor knows it is finished and won't
+    // receive more incoming tasks to run.
+    drop(spawner);
+
+    // Run the executor until the task queue is empty.
+    // This will print "howdy!", pause, and then print "done!".
+    executor.run();
+}
